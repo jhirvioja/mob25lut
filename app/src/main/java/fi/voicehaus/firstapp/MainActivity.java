53da8a3800ec4697a35da2c3ef2d67af52726eb5
Jaakko Hirvioja <jhirvioja@gmail.com>
2025-01-18 08:07:49 +0200

    Add second activity, button logic to main activity

--- app/src/main/java/fi/voicehaus/firstapp/MainActivity.java
+++ app/src/main/java/fi/voicehaus/firstapp/MainActivity.java
@@ -1,6 +1,9 @@
 package fi.voicehaus.firstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+
+                startActivity(startIntent);
+            }
+        });
     }
 }
