0fb3c73673f39b3a75a514063feedcdf294c2a7b
Jaakko Hirvioja <jhirvioja@gmail.com>
2025-01-18 08:52:16 +0200

    Add functionality to open google from main activity

--- app/src/main/java/fi/voicehaus/firstapp/MainActivity.java
+++ app/src/main/java/fi/voicehaus/firstapp/MainActivity.java
@@ -1,6 +1,7 @@
 package fi.voicehaus.firstapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,20 @@ public class MainActivity extends AppCompatActivity {
                 Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
                 startIntent.putExtra("fi.voicehaus.firstapp.SOMETHING", "HELLO WORLD!");
                 startActivity(startIntent);
+            }
+        });
+
+        Button googleBtn = (Button) findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "http://www.google.com";
+                Uri webaddress = Uri.parse(google);
+
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webaddress);
+                if (gotoGoogle.resolveActivity(getPackageManager()) != null) {
+                    startActivity(gotoGoogle);
+                }
             }
         });
     }
