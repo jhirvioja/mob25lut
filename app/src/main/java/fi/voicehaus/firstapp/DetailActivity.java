4c937aa2bcd55d0303cb4d4e2927caa9afb30b80
Jaakko Hirvioja <jhirvioja@gmail.com>
2025-01-19 11:52:09 +0200

    Add images, image viewing logic w/ scaling, prettify

--- app/src/main/java/fi/voicehaus/firstapp/DetailActivity.java
+++ app/src/main/java/fi/voicehaus/firstapp/DetailActivity.java
@@ -1,6 +1,11 @@
 package fi.voicehaus.firstapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +15,46 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent in = getIntent();
+        int index = in.getIntExtra("fi.voicehaus.firstapp.ITEM_INDEX", -1);
+
+        if (index > -1) {
+            int pic = getImg(index);
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            scaleImg(img, pic);
+        }
     }
+
+    private int getImg(int index) {
+        switch (index) {
+            case 0:
+                return R.drawable.peach;
+            case 1:
+                return R.drawable.tomato;
+            case 2:
+                return R.drawable.squash;
+            default:
+                return -1;
+        }
+    }
+
+    private void scaleImg(ImageView img, int pic) {
+        Display screen = getWindowManager().getDefaultDisplay();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), pic, options);
+
+        int imgWidth = options.outWidth;
+        int screenWidth = screen.getWidth();
+
+        if (imgWidth > screenWidth) {
+            options.inSampleSize = Math.round((float) imgWidth / (float) screenWidth);
+        }
+        options.inJustDecodeBounds = false;
+        Bitmap scaledImg = BitmapFactory.decodeResource(getResources(), pic, options);
+        img.setImageBitmap(scaledImg);
+    }
+
 }
